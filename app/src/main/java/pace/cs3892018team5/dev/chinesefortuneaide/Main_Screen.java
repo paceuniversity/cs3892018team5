7195c1c89e2524a214d2cf7ef2adad1a8a9a8437
JIachengChen <35778166+JIachengChen@users.noreply.github.com>
2018-03-23 14:10:24 -0400

    Initial commit

---
+++ app/src/main/java/pace/cs3892018team5/dev/chinesefortuneaide/Main_Screen.java
@@ -0,0 +1,13 @@
+package pace.cs3892018team5.dev.chinesefortuneaide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__screen);
+    }
+}
