3ab01b56108b4029339a50308345319459078380
Amir <admodo@gmail.com>
2018-03-30 16:45:52 -0400

    Created mainscreen and tabs that can swipe.

--- app/src/main/java/pace/cs3892018team5/dev/chinesefortuneaide/Main_Screen.java
+++ app/src/main/java/pace/cs3892018team5/dev/chinesefortuneaide/Main_Screen.java
@@ -1,13 +1,53 @@
 package pace.cs3892018team5.dev.chinesefortuneaide;
 
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
-public class Main_Screen extends AppCompatActivity {
+public class Main_Screen extends AppCompatActivity implements  Tab1.OnFragmentInteractionListener,Tab2.OnFragmentInteractionListener,Tab3.OnFragmentInteractionListener, Tab4.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__screen);
+
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Fortune "));
+        tabLayout.addTab(tabLayout.newTab().setText("Word "));
+        tabLayout.addTab(tabLayout.newTab().setText("Fact"));
+        tabLayout.addTab(tabLayout.newTab().setText("Quiz"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
