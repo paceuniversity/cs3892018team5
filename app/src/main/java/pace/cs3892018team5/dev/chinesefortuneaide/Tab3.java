8cd56f52155817843f074ca29c45fa5f47ee3bd8
JIachengChen <35778166+JIachengChen@users.noreply.github.com>
2018-04-02 19:38:37 -0400

    word class & fact class. example for Word & Fact

--- app/src/main/java/pace/cs3892018team5/dev/chinesefortuneaide/Tab3.java
+++ app/src/main/java/pace/cs3892018team5/dev/chinesefortuneaide/Tab3.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
@@ -22,6 +23,10 @@ public class Tab3 extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    ListView listViewWord;
+
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,6 +63,7 @@ public class Tab3 extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
---
+++ app/src/main/java/pace/cs3892018team5/dev/chinesefortuneaide/fact.java
@@ -0,0 +1,17 @@
+package pace.cs3892018team5.dev.chinesefortuneaide;
+
+/**
+ * Created by johnnychen on 2018/4/2.
+ */
+
+public class fact {
+    String content;
+
+    public fact(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
